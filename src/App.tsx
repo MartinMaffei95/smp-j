22b333749c492a4a0f1453b3a75a30b4564096b1
Your Name Martin <you@example.comMartinMaffei95@gmail.com>
2023-02-04 00:18:51 -0300

    changed appid

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,30 @@ function App() {
   const [loged, setLoged] = useState<boolean>(false);
 
   const fbLogin = () => {
-    FB.login(function (response) {
-      console.log(response);
-      if (response.authResponse) {
-        console.log('Welcome!  Fetching your information.... ');
-        FB.api('/me', function (response) {
-          // console.log('Good to see you, ' + response + '.');
-          console.log('res of FBAPI:', response);
-        });
-      } else {
-        console.log('User cancelled login or did not fully authorize.');
+    FB.login(
+      function (response) {
+        console.log(response);
+        if (response.authResponse) {
+          console.log('Welcome!  Fetching your information.... ');
+          FB.api('/me', function (response) {
+            // console.log('Good to see you, ' + response + '.');
+            console.log('res of FBAPI:', response);
+          });
+        } else {
+          console.log('User cancelled login or did not fully authorize.');
+        }
+      },
+      {
+        scope:
+          'pages_show_list,instagram_basic,pages_read_user_content,public_profile',
       }
+    );
+  };
+  const logOut = () => {
+    window?.FB?.logout((res) => {
+      console.log(res);
     });
+    setLoged(false);
   };
 
   useEffect(() => {
@@ -53,15 +65,19 @@ function App() {
 
   return (
     <div className="App p-4">
-      <div>{loged ? 'loged' : 'NotLoged'}</div>
       <div>
-        <button
-          className="bg-blue-400 p-2 text-white font-bold"
-          onClick={() => fbLogin()}
-        >
-          Log In
-        </button>
+        {loged ? (
+          <button onClick={logOut}>Cerrar Sesion</button>
+        ) : (
+          <button
+            className="bg-blue-400 p-2 text-white font-bold"
+            onClick={() => fbLogin()}
+          >
+            Log In
+          </button>
+        )}
       </div>
+      <div></div>
     </div>
   );
 }
