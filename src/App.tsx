9ce2ff628ec46d4a4882db61c3fbeb68c49b534e
Your Name Martin <you@example.comMartinMaffei95@gmail.com>
2023-02-04 00:03:47 -0300

    First login

--- src/App.tsx
+++ src/App.tsx
@@ -3,31 +3,37 @@ import reactLogo from './assets/react.svg';
 
 function App() {
   const [loged, setLoged] = useState<boolean>(false);
+
   const fbLogin = () => {
     FB.login(function (response) {
-      if (response.status === 'connected') {
-        // Logged into your webpage and Facebook.
+      console.log(response);
+      if (response.authResponse) {
+        console.log('Welcome!  Fetching your information.... ');
+        FB.api('/me', function (response) {
+          // console.log('Good to see you, ' + response + '.');
+          console.log('res of FBAPI:', response);
+        });
       } else {
-        // The person is not logged into your webpage or we are unable to tell.
+        console.log('User cancelled login or did not fully authorize.');
       }
     });
   };
 
   useEffect(() => {
     window.fbAsyncInit = function () {
-      window?.FB?.init({
+      FB?.init({
         appId: '709846977358878',
         cookie: true,
         xfbml: true,
         version: 'v16.0',
       });
 
-      window?.FB?.AppEvents.logPageView();
+      // FB.AppEvents.logPageView();
 
-      window?.FB?.getLoginStatus(function (response) {
+      FB.getLoginStatus(function (response) {
         console.log(response);
-        if (response.status === 'connected') setLoged(true);
-        else setLoged(false);
+        // if (response.status === 'connected') setLoged(true);
+        // else setLoged(false);
       });
     };
 
@@ -50,7 +56,7 @@ function App() {
       <div>
         <button
           className="bg-blue-400 p-2 text-white font-bold"
-          onClick={fbLogin}
+          onClick={() => fbLogin()}
         >
           Log In
         </button>
